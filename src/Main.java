0ef2808e085cdabbc5bc7c5b60a77296b9982b54
kores10 <kores10@email.cz>
2022-11-06 17:26:48 +0100

    4. commit - dosud na lokalnim repo, návrat k jedné řádce

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Hello country!");
-        System.out.println("Hello universe!");
-        System.out.println("Hello every and each body!");
 
     }
 }
